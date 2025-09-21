3e85264e4d908cecaa698b3ca2d010d2b677c34e
iammiguelsolis <miguelsoliscunza24@gmail.com>
2025-09-21 01:28:33 -0500

    v3

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,3 +1,4 @@
+import { cn } from '@/utils/cn';
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 
@@ -10,7 +11,7 @@ type Props = {
 
 export default function SearchBox(props: Props) {
   return (
-    <form onSubmit={props.onSubmit} className='flex relative items-center justifiy-center h-10'>
+    <form onSubmit={props.onSubmit} className={cn('flex relative items-center justifiy-center h-10', props.className)}>
       <input value={props.value} type="text" onChange={props.onChange} placeholder='Search location...' className='px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full'/>
       <button className='px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 whitespace-nowrap h-full'>
         <IoSearch />
